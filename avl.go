f859fa24a692c578a84b9704daabb821bb8eb61f
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-10-07 14:27:30 +0200

    avl
    nevyvazuje

--- avl.go
+++ avl.go
@@ -25,3 +25,54 @@ func (v *vertex) String() (out string) {
 	out += ")"
 	return
 }
+
+func (v *vertex) min() *vertex {
+	if v == nil {
+		return v
+	} else {
+		return v.left.min()
+	}
+}
+
+func (v *vertex) max() *vertex {
+	if v == nil {
+		return v
+	} else {
+		return v.right.max()
+	}
+}
+
+func (v *vertex) insert(p plate) *vertex { // hloupy insert
+	if v == nil {
+		v = &vertex{}
+		v.value = p
+	} else if p.value < v.value.value {
+		v.left = v.left.insert(p)
+	} else if p.value > v.value.value {
+		v.right = v.right.insert(p)
+	}
+	return v
+}
+
+func (v *vertex) delete(value uint) *vertex { // hloupý delete
+	if value == v.value.value {
+		return nil
+	} else if value < v.value.value {
+		v.left = v.left.delete(value)
+	} else if value > v.value.value {
+		v.right = v.right.delete(value)
+	} else if value == v.value.value {
+		if v.right == nil && v.left == nil {
+			return nil
+		} else if v.right == nil {
+			return v.left
+		} else if v.left == nil {
+			return v.right
+		} else {
+			tmp := v.right.min()
+			v.value = tmp.value
+			v.right = v.right.delete(tmp.value.value)
+		}
+	}
+	return v
+}
--- mimozenstan.go
+++ mimozenstan.go
@@ -6,18 +6,22 @@ func main() {
 	var T uint
 	fmt.Scan(&T)
 	for i := uint(0); i < T; i++ {
-		var N uint
+		var N, Ni uint
 		var l, a *element
+		var avl *vertex
 		l = &element{}
 		a = l
 
 		fmt.Scan(&N)
-		fmt.Scan(&l.value)
+		fmt.Scan(&Ni)
+		l.value = Ni
+		avl = avl.insert(plate{Ni, l})
 		for j := uint(1); j < N; j++ {
-			var Ni uint
 			fmt.Scan(&Ni)
 			a = a.append(Ni)
+			avl = avl.insert(plate{Ni, a})
 		}
-		fmt.Println(l.String())
+		fmt.Println(l)
+		fmt.Println(avl)
 	}
 }
